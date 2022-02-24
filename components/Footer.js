0617c4547c4d52adc65e480cb09a8f225edbc1d3
HOSENUR <nur08439@gmail.com>
2022-02-25 01:36:10 +0530

    Added Google Analytics

--- components/Footer.js
+++ components/Footer.js
@@ -12,10 +12,9 @@ export default function Footer() {
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-        <Link href="/">{siteMetadata.title}</Link>
+          <Link href="/">{siteMetadata.title}</Link>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-          
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
